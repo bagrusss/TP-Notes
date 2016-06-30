f306abf414f8db34a01292f006910884876ea718
bagrusss <bagrusss@gmail.com>
2016-07-01 02:30:20 +0300

    add icons, EditActivity

--- app/src/main/java/ru/bagrusss/tpnotes/activities/EditActivity.java
+++ app/src/main/java/ru/bagrusss/tpnotes/activities/EditActivity.java
@@ -1,15 +1,75 @@
 package ru.bagrusss.tpnotes.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import ru.bagrusss.tpnotes.R;
 
 public class EditActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private boolean mLockState = false;
+    private MenuItem mLockItem;
+    private EditText mNoteText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNoteText = (EditText) findViewById(R.id.note_text);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent internalIntent = getIntent();
+        mLockState = internalIntent.getBooleanExtra("lock", false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        mLockItem = menu.findItem(R.id.lock_action);
+        setLockIcon(mLockState);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean res = true;
+        switch (item.getItemId()) {
+            case R.id.lock_action:
+                mLockState = !mLockState;
+                setLockIcon(mLockState);
+                break;
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.share_action:
+
+                break;
+            default:
+                res = false;
+        }
+        return res;
+    }
+
+    @Override
+    public void onBackPressed() {
+        save();
+        super.onBackPressed();
+    }
+
+    private void setLockIcon(boolean lock) {
+        mLockItem.setIcon(lock ?
+                R.drawable.ic_lock_outline_white_24dp :
+                R.drawable.ic_lock_open_white_24dp);
+    }
+
+    private void save() {
+
     }
 }
--- app/src/main/java/ru/bagrusss/tpnotes/activities/NotesActivity.java
+++ app/src/main/java/ru/bagrusss/tpnotes/activities/NotesActivity.java
@@ -9,10 +9,12 @@ import android.support.v7.widget.Toolbar;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 
 import ru.bagrusss.tpnotes.R;
 import ru.bagrusss.tpnotes.fragments.AboutFragment;
+import ru.bagrusss.tpnotes.fragments.CategoriesFragment;
 import ru.bagrusss.tpnotes.fragments.NotesFragment;
 import ru.bagrusss.tpnotes.fragments.SettingsFragment;
 
@@ -24,8 +26,9 @@ public class NotesActivity extends AppCompatActivity {
     private int mLastPosition;
 
     private static final int NOTES_POSITION = 1;
-    private static final int SETTINGS_POSITION = 2;
-    private static final int ABOUT_POSITION = 3;
+    private static final int CATEGORIES_POSITION = 3;
+    private static final int SETTINGS_POSITION = 5;
+    private static final int ABOUT_POSITION = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,7 @@ public class NotesActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         mFragmentManager = getFragmentManager();
-
+        mFragmentManager.beginTransaction().replace(R.id.container, new NotesFragment()).commit();
         buildDrawer();
     }
 
@@ -44,10 +47,15 @@ public class NotesActivity extends AppCompatActivity {
                 .withToolbar(mToolbar)
                 .withAccountHeader(new AccountHeaderBuilder().withActivity(this).withHeaderBackground(R.drawable.drawer_light).build())
                 .withDisplayBelowStatusBar(true)
+                .withActionBarDrawerToggleAnimated(true)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.notes),
-                        new PrimaryDrawerItem().withName(R.string.settings),
-                        new PrimaryDrawerItem().withName(R.string.about)
+                        new PrimaryDrawerItem().withName(R.string.notes).withIcon(R.drawable.ic_assignment_black_24dp),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName(R.string.categories).withIcon(R.drawable.ic_folder_black_24dp),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName(R.string.settings).withIcon(R.drawable.ic_settings_black_24dp),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName(R.string.about).withIcon(R.drawable.ic_info_black_24dp)
                 ).withOnDrawerItemClickListener((view, position, drawerItem) -> {
                     mLastPosition = position;
                     int resTitle = 0;
@@ -65,6 +73,9 @@ public class NotesActivity extends AppCompatActivity {
                             resTitle = R.string.about;
                             fragment = new AboutFragment();
                             break;
+                        case CATEGORIES_POSITION:
+                            resTitle = R.string.categories;
+                            fragment = new CategoriesFragment();
                         default:
                             return true;
                     }
