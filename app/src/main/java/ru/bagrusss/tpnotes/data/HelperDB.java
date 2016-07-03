01bce7481d2e6f3beca4323b54b27be493e6a11c
bagrusss <bagrusss@gmail.com>
2016-07-04 02:09:06 +0300

    fix

--- app/src/main/java/ru/bagrusss/tpnotes/data/HelperDB.java
+++ app/src/main/java/ru/bagrusss/tpnotes/data/HelperDB.java
@@ -73,7 +73,9 @@ public class HelperDB extends SQLiteOpenHelper {
         db.execSQL(CREATE_CATEGORIES);
         ContentValues cv = new ContentValues();
         cv.put(NAME, "");
-        cv.put(COLOR, "#00000000");
+        db.insert(TABLE_CATEGORIES, null, cv);
+        cv.put(NAME, "temp");
+        cv.put(COLOR, "#00FFFF");
         db.insert(TABLE_CATEGORIES, null, cv);
     }
 
